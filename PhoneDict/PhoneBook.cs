8d364f04ad01cbff6ad2efdd03d5426b8cef082d
miecz <miecz@LAPTOP-UVM3H6HN>
2022-11-18 11:09:28 +0100

    Add, Display contact from phone book

---
+++ PhoneDict/PhoneBook.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PhoneDict
+{
+    class PhoneBook
+    {
+        public List<Contact> Contacts { get; set; }
+
+        public void AddContact(Contact contact)
+        {
+            Contacts.Add(contact);
+        }
+
+        public void DisplayContact(string number)
+        {
+            var contact = Contacts.FirstOrDefault(c => c.Number == number);
+
+            if (contact == null)
+            {
+                Console.WriteLine("Contact not found");
+            }
+            else
+            {
+                Console.WriteLine($"Contact: {contact.Name}, {contact.Number}");
+            }
+        }
+    }
+}
